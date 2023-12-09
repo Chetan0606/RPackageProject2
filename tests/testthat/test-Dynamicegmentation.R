e34e5eb2f0bd381ad13413b01f3402a6eb5b8664
Chetan Sunku <chetansunku@gmail.com>
2023-12-08 21:27:41 -0700

    Updated test file

--- tests/testthat/test-Dynamicegmentation.R
+++ tests/testthat/test-Dynamicegmentation.R
@@ -7,14 +7,7 @@ test_that("dynamicSegmentation_algo computes the same answer as R", {
   data <- rnorm(100)
   threshold = 0.5
   dynamicsegmentation <- dynamicSegmentation_algo(data, threshold)
-  expect_equal(dynamicsegmentation$segment_indices, 1)
-  expect_equal(dynamicsegmentation$segment_means, 0.09040591)
   
-  # Check if the length is 1 for segment_indices and segment_means
-  expect_equal(length(dynamicsegmentation$segment_indices), 1)
-  expect_equal(length(dynamicsegmentation$segment_means), 1)
-  
-  # Check the actual values
   expect_equal(dynamicsegmentation$segment_indices, 1)
   expect_equal(dynamicsegmentation$segment_means, 0.09040591, tolerance = 1e-6)
   
